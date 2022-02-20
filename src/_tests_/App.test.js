ff27d63479d547c08caa23bfa0882acb4ec6aa4b
Molochnyk <slapibs18@gmail.com>
2022-02-20 10:38:41 +0200

    add start tests

---
+++ src/_tests_/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import CurrencyConverter from '../pages/CurrencyConverter';
+import CurrencyList from '../pages/CurrencyList';
+
+describe("CurrencyConverter", () => {
+ it("renders CurrencyConverter component", () => {
+  render(<CurrencyConverter/>);
+
+  expect(screen.getByText(/Курс Обмена/i)).toBeInTheDocument();
+ });
+});
+
+describe("CurrencyList", () => {
+  it("renders CurrencyList component", async () => {
+   render(<CurrencyList/>);
+
+   expect(screen.getByText(/Курс валют/i)).toBeInTheDocument();
+    
+   expect(await screen.findByRole('table')).toBeInTheDocument();
+  });
+ });
