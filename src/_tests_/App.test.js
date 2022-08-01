942c9e4fb7d1b9095b9d1a93a43adbeaae9cd6fe
Molochnyk <slapibs18@gmail.com>
2022-08-01 12:23:23 +0300

    feature added sorting to the currency page

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import CurrencyConverter from '../pages/CurrencyConverter';
 import CurrencyList from '../pages/CurrencyList';
 
-describe("CurrencyConverter", () => {
- it("renders CurrencyConverter component", () => {
-  render(<CurrencyConverter/>);
+describe('CurrencyConverter', () => {
+  it('renders CurrencyConverter component', () => {
+    render(<CurrencyConverter />);
 
-  expect(screen.getByText(/Курс Обмена/i)).toBeInTheDocument();
- });
+    expect(screen.getByText(/Курс Обмена/i)).toBeInTheDocument();
+  });
 });
 
-describe("CurrencyList", () => {
-  it("renders CurrencyList component", async () => {
-   render(<CurrencyList/>);
+describe('CurrencyList', () => {
+  it('renders CurrencyList component', async () => {
+    render(<CurrencyList />);
 
-   expect(screen.getByText(/Курс валют/i)).toBeInTheDocument();
-    
-   expect(await screen.findByRole('table')).toBeInTheDocument();
+    expect(screen.getByText(/Курс валют/i)).toBeInTheDocument();
+
+    expect(await screen.findByRole('table')).toBeInTheDocument();
   });
- });
+});
